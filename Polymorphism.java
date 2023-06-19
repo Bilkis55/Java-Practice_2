d74dde9f26227fbba8a21e37ea2c42f209076551
Bilkis55 <133634469+Bilkis55@users.noreply.github.com>
2023-06-19 18:23:26 +0530

    Add files via upload
    polymorphism practice

---
+++ Polymorphism.java
@@ -0,0 +1,40 @@
+//java polymorphism
+class Polygon
+{
+    //method to render a shape
+    public void render()
+    {
+        System.out.println("Rendering Polygon...");
+    }
+}
+class Square extends Polygon
+{
+    //render square
+    public void render()
+    {
+        System.out.println("Rendering Square...");
+    }
+    
+}
+class Circle extends Polygon
+{
+    //render circle
+    public void render()
+    {
+        System.out.println("Rendering of Circle...");
+    }
+}
+ class Polymorphism
+ {
+    public static void main(String[]args)
+    {
+        //create an object of square
+        Square s1=new Square();
+        s1.render();
+        //create an object of Circle
+        Circle c1=new Circle();
+        c1.render();
+        
+    }
+    
+}
